76b9430664789c5c5f5908785a1e6329feff6e62
phk422 <penghongkun422@gmail.com>
2024-12-27 14:50:13 +0800

    chore: rolldown配置vue的支持

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,20 +1,47 @@
 import { defineConfig } from "rolldown";
 import clear from "rollup-plugin-clear";
 import replace from "@rollup/plugin-replace";
+import progress from "rollup-plugin-progress";
+import vue from "@vitejs/plugin-vue";
+import copy from "rollup-plugin-copy";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
 
 export default defineConfig([
   {
-    input: 'src/index.js',
+    input: "src/main.js",
     output: {
-      format: "iife",
+      dir: "dist",
+      // format: "esm",
+      sourcemap: true,
+      advancedChunks: {
+        groups: [
+          {
+            name: "vue",
+          },
+        ],
+      },
     },
     plugins: [
+      vue(),
       clear({
-        targets: ['dist']
+        targets: ["dist"],
       }),
       replace({
-        "FOO": JSON.stringify("我是注入的变量")
+        FOO: JSON.stringify("我是注入的变量"),
       }),
-    ]
-  }
-])
+      copy({
+        targets: [{ src: "public/**", dest: "dist" }],
+      }),
+      progress(),
+      // 热加载
+      livereload(),
+      // 本地服务
+      serve({
+        contentBase: "dist",
+        port: 4576,
+        open: true
+      }),
+    ],
+  },
+]);
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { hello } from './hello'
-
-hello()
-
-console.log(FOO);
